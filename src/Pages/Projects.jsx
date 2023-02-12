3198b6d53a50859b2911ee36d96c1a8556ebc4fd
varun8177 <varunergurala9999@gmail.com>
2023-02-12 19:44:28 +0530

    update Projects.jsx

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,7 +1,21 @@
+import { Box, Center, Heading } from "@chakra-ui/react";
 import React from "react";
+import ProjectCards from "../components/Projects/ProjectCards";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <Box pl={20} pr={20}>
+      <Center>
+        <Heading mb={"40px"} fontSize={"62px"}>
+          Projects
+        </Heading>
+      </Center>
+      <ProjectCards direction={"row"} />
+      <ProjectCards direction={"row-reverse"} />
+      <ProjectCards direction={"row"} />
+      <ProjectCards direction={"row-reverse"} />
+    </Box>
+  );
 };
 
 export default Projects;
