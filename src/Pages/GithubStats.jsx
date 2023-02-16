941d670734b8f62644edf07b8c801668131c492a
varun8177 <varunergurala9999@gmail.com>
2023-02-16 19:14:24 +0530

    update GithubStats.jsx

--- src/Pages/GithubStats.jsx
+++ src/Pages/GithubStats.jsx
@@ -14,7 +14,7 @@ const GithubStats = () => {
         {/* Calender */}
         <GitHubCalendar
           class="react-activity-calendar"
-          style={{ margin: "auto" }}
+          style={{ margin: "auto", width: "80%" }}
           username="Varun8177"
           blockSize={20}
           fontSize={20}
@@ -22,7 +22,18 @@ const GithubStats = () => {
           <Tooltip delayShow={20} html />
         </GitHubCalendar>
         {/* Stats */}
-        <Flex w={"100%"} justifyContent={"space-evenly"} alignItems={"center"}>
+        <Flex
+          w={"100%"}
+          justifyContent={"space-evenly"}
+          alignItems={"center"}
+          direction={{
+            sm: "column",
+            md: "column",
+            lg: "column",
+            xl: "row",
+            "2xl": "row",
+          }}
+        >
           <Link
             m={"auto"}
             to={
@@ -30,18 +41,33 @@ const GithubStats = () => {
             }
           >
             <Image
+              w={{
+                sm: "50%",
+                md: "50%",
+                lg: "70%",
+                xl: "80%",
+                "2xl": "100%",
+              }}
+              m={"auto"}
               id="github-streak-stats"
               src="https://github-readme-streak-stats.herokuapp.com?user=Varun8177"
             />
           </Link>
           {/* Top language */}
           <Link
-            m={"auto"}
             to={
               "https://github-readme-stats.vercel.app/api/top-langs/?username=Varun8177&layout=compact"
             }
           >
             <Image
+              w={{
+                sm: "50%",
+                md: "50%",
+                lg: "70%",
+                xl: "80%",
+                "2xl": "100%",
+              }}
+              m={"auto"}
               id="github-top-langs"
               src="https://github-readme-stats.vercel.app/api/top-langs/?username=Varun8177&layout=compact"
             />
@@ -53,6 +79,14 @@ const GithubStats = () => {
             to={"https://github-readme-stats.vercel.app/api?username=Varun8177"}
           >
             <Image
+              w={{
+                sm: "50%",
+                md: "50%",
+                lg: "70%",
+                xl: "80%",
+                "2xl": "100%",
+              }}
+              m={"auto"}
               id="github-stats-card"
               src="https://github-readme-stats.vercel.app/api?username=Varun8177&count_private=true&show_icons=true"
             />
