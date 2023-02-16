66b9957dfeebdef2b708a2280f057fc9cc1f1144
varun8177 <varunergurala9999@gmail.com>
2023-02-16 19:15:00 +0530

    update Projects.jsx

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,6 +1,10 @@
-import { Box, Center, Heading, useColorMode } from "@chakra-ui/react";
+import { Box, Center, Heading, Show, useColorMode } from "@chakra-ui/react";
 import React from "react";
-import ProjectCards from "../components/Projects/ProjectCards";
+import {
+  MediumProjectCard,
+  MobileProjectCard,
+  ProjectCards,
+} from "../components/Projects/ProjectCards";
 
 const Projects = () => {
   return (
@@ -14,6 +18,18 @@ const Projects = () => {
       <ProjectCards direction={"row-reverse"} />
       <ProjectCards direction={"row"} />
       <ProjectCards direction={"row-reverse"} />
+      <Show breakpoint="(max-width: 760px)">
+        <MobileProjectCard direction={"row"} />
+        <MobileProjectCard direction={"row-reverse"} />
+        <MobileProjectCard direction={"row"} />
+        <MobileProjectCard direction={"row-reverse"} />
+      </Show>
+      <Show breakpoint="(max-width: 993px)">
+        <MediumProjectCard />
+        <MediumProjectCard />
+        <MediumProjectCard />
+        <MediumProjectCard />
+      </Show>
     </Box>
   );
 };
